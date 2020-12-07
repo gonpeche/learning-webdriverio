16b8898693bff623317a5048866fbb3ee5041f8a
gonpeche <espeche.g@gmail.com>
2020-12-07 19:59:13 -0300

    expect browser toHaveTitle

--- tests/example.js
+++ tests/example.js
@@ -1,5 +1,7 @@
 describe('First test', () => {
     it('Load Example Website', () => {
         browser.url('https://grid.layoutit.com/')
+        browser.pause(3000)
+        expect(browser).toHaveTitle('Interactive CSS Grid Generator | Layoutit Grid')
     })
 })
