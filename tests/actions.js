70dcd16d8196f4e545d388360146c03f8454ed01
gonpeche <espeche.g@gmail.com>
2020-12-07 22:52:33 -0300

    browser actions

---
+++ tests/actions.js
@@ -0,0 +1,42 @@
+// import { short } from '../lib/timeouts'
+
+describe('Browser Actions', () => {
+    it('Inputs', () => {
+        browser.url('https://devexpress.github.io/testcafe/example/')
+        const input = $('#developer-name')
+        input.waitForExist()
+        input.setValue('Mike')
+        browser.pause(short)
+        input.clearValue()
+        browser.pause(short)
+        input.addValue('Mike')
+        browser.pause(short)
+    })
+
+    it('Click', () => {
+        const buttonPupulate = $('#populate')
+        buttonPupulate.waitForExist()
+        buttonPupulate.click()
+    })
+
+    it('Checkbox & Radio Button', () => {
+        browser.url('https://devexpress.github.io/testcafe/example/')
+        const radio = $('#linux')
+        radio.waitForExist()
+        radio.click()
+        browser.pause(short)
+
+        const checkbox = $('#remote-testing')
+        checkbox.waitForExist()
+        checkbox.click()
+        browser.pause(short)
+    })
+
+    it('Select Box', () => {
+        browser.url('https://devexpress.github.io/testcafe/example/')
+        const select = $('#preferred-interface')
+        select.waitForExist()
+        select.selectByVisibleText('JavaScript API')
+        browser.pause(short)
+    })
+})
--- tests/example.js
+++ tests/example.js
@@ -1,7 +1,41 @@
-describe('First test', () => {
-    it('Load Example Website', () => {
-        browser.url('https://grid.layoutit.com/')
-        browser.pause(3000)
-        expect(browser).toHaveTitle('Interactive CSS Grid Generator | Layoutit Grid')
+import { short, long } from '../lib/timeouts'
+import { mobile, tablet, desktop } from '../lib/devices'
+
+describe('First Steps with WebdrverIO', () => {
+    it('Assert Attribute', () => {
+        browser.url('https://devexpress.github.io/testcafe/example/')
+        const button = $('#submit-button')
+        button.waitForExist()
+        expect(button).toHaveAttrContaining('type', 'submit')
+    })
+
+    it('Assert Value', () => {
+        const button = $('#populate')
+        button.waitForExist()
+        expect(button).toHaveValue('Populate')
+    })
+
+    it('Save Screenshot', () => {
+        browser.saveScreenshot('your-name.png')
+    })
+
+    it('Change Browser Viewport', () => {
+        browser.setWindowSize(800, 600)
+        browser.pause(long)
+    })
+
+    it('Set Mobile View', () => {
+        browser.setWindowSize(mobile[0], mobile[1])
+        browser.pause(short)
+    })
+
+    it('Set Tablet View', () => {
+        browser.setWindowSize(tablet[0], tablet[1])
+        browser.pause(short)
+    })
+
+    it('Set Desktop View', () => {
+        browser.setWindowSize(desktop[0], desktop[1])
+        browser.pause(short)
     })
 })
